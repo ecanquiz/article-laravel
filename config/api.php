53450e5b1fe6f839d0b6baeeebba87eb0b4e7564
Ernesto Canquiz <caribestic123@gmail.com>
2025-07-17 12:31:16 -0400

    feat: Add the client name in the configuration

--- config/api.php
+++ config/api.php
@@ -6,6 +6,7 @@ return [
     ],
     'static_assets' => [
         'url' => env('API_STATIC_ASSETS_URL', 'http://localhost'),
+        'client_name' => env('API_STATIC_ASSETS_CLIENT_NAME', 'client_name'),
         'shared_token' => env('API_STATIC_ASSETS_SHARED_TOKEN', 'secret_token')     
     ]
 ];
